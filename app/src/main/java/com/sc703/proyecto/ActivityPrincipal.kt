7d5c4e3d71671b4aa2a0632a27e7357d0b05af98
yeco <yeisonpa.96@hotmail.com>
2021-08-23 22:48:40 -0600

    cambiar imagen

--- app/src/main/java/com/sc703/proyecto/ActivityPrincipal.kt
+++ app/src/main/java/com/sc703/proyecto/ActivityPrincipal.kt
@@ -1,7 +1,13 @@
 package com.sc703.proyecto
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,12 +17,24 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.sc703.proyecto.databinding.ActivityPrincipalBinding
+import com.sc703.proyecto.ui.gallery.Usuario
+import java.io.File
+import java.io.IOException
 
 class ActivityPrincipal : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityPrincipalBinding
+
+    private lateinit var textViewCORREO: TextView
+    private lateinit var imv_imagen: ImageView
+    private lateinit var usuario: Usuario
+    private lateinit var Almacenamiento: StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +44,10 @@ class ActivityPrincipal : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarActivityPrincipal.toolbar)
 
+//        val navigationView : NavigationView  = findViewById(R.id.nav_view)
+//        val headerView : View = navigationView.rootView
+//
+
 //        binding.appBarActivityPrincipal.fab.setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show()
@@ -33,6 +55,18 @@ class ActivityPrincipal : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_activity_principal)
+
+        val headerView : View = navView.getHeaderView(0)
+        textViewCORREO  = headerView.findViewById(R.id.textViewCORREO)
+        imv_imagen  = headerView.findViewById(R.id.imageViewUSUARIO)
+
+        usuario = Usuario()
+        val user = Firebase.auth.currentUser
+        user?.let {
+            usuario.correo = user.email.toString()
+        }
+        DescargaArchivo()
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
@@ -42,6 +76,38 @@ class ActivityPrincipal : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        textViewCORREO.text = usuario.correo
+    }
+
+
+
+    private fun DescargaArchivo() {
+        usuario = Usuario()
+        val user = Firebase.auth.currentUser
+        user?.let {
+            usuario.id = user.uid
+        }
+
+        //Asignamos la ruta de la imagen a descargar
+        Almacenamiento = FirebaseStorage.getInstance().reference.child(
+            "Imagenes/" + usuario.id.toString()
+        )
+
+        lateinit var temporal: File
+
+        try {
+            temporal = File.createTempFile("images", ".jpg")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        val archivoFinal = temporal
+        Almacenamiento.getFile(temporal)
+            .addOnSuccessListener {
+                val imagen = archivoFinal.absolutePath
+                imv_imagen.setImageBitmap(BitmapFactory.decodeFile(imagen))
+            }
+            .addOnFailureListener {
+            }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
